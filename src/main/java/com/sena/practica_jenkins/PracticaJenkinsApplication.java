2fdb094cd896ad23d567af9f322e5012ed34ce0a
JohanAceroSalazar <johanacero2509@gmail.com>
2026-05-21 10:24:23 -0500

    Correcion jenkinsfile

--- src/main/java/com/sena/practica_jenkins/PracticaJenkinsApplication.java
+++ src/main/java/com/sena/practica_jenkins/PracticaJenkinsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracticaJenkinsApplication 
+public class PracticaJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaJenkinsApplication.class, args);
